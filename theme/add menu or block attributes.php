44e37419fc10cc12e02c048085072f85f1dac5b7
Polar Bottle <shenxinxu@Shens-MacBook-Pro.local>
2016-06-09 11:43:21 -0400

    update

---
+++ theme/add menu or block attributes.php
@@ -0,0 +1,10 @@
+<?php 
+
+
+// This can be acheived by using third party modules
+/*
+
+	drush dl menu_attributes
+	drush dl block_attributes
+
+*/
---
+++ theme/hide menu title.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+// Once you created your menu, that has in fact created a Block.
+/*
+So now , in your Site Building > Blocks > List go to the Menu Block you just created and hit Configure ... now, there, change the Block title (it's empty at first) to < none > (without the spaces).
+
+Title shoud be gone now.
+*/
